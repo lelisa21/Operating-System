c6dab5692bfb1f43cf2ca6bcb7c6324765ee7156
lelisa21 <lelisahailu21@gmail.com>
2026-05-11 09:57:03 +0300

    feat: FCFS and SJF algorithms implementation

--- src/algorithms/fcfs.c
+++ src/algorithms/fcfs.c
@@ -1,1 +1,72 @@
-// 
+#include "../../include/scheduler.h"
+
+void fcfs(Process p[], int n)
+{
+    int i, j;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            int should_swap = 0;
+
+            // Sort by arrival time 
+            if (p[j].arrival_time > p[j + 1].arrival_time)
+            {
+                should_swap = 1;
+            }
+
+            // Tie breaker: smaller PID first 
+            else if (p[j].arrival_time ==
+                     p[j + 1].arrival_time)
+            {
+                if (p[j].pid > p[j + 1].pid)
+                {
+                    should_swap = 1;
+                }
+            }
+
+            if (should_swap)
+            {
+                Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
+
+
+    int current_time = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        // CPU idle until process arrives 
+        if (current_time < p[i].arrival_time)
+        {
+            current_time = p[i].arrival_time;
+        }
+
+        // Response time 
+        p[i].response_time =
+            current_time - p[i].arrival_time;
+
+        // Waiting time 
+        p[i].waiting_time =
+            current_time - p[i].arrival_time;
+
+        // Execute process 
+        current_time += p[i].burst_time;
+
+        p[i].completion_time = current_time;
+
+        // Turnaround time 
+        p[i].turnaround_time =
+            p[i].completion_time -
+            p[i].arrival_time;
+
+        // Process finished 
+        p[i].remaining_time = 0;
+        p[i].started = 1;
+    }
+}
--- src/algorithms/sjf.c
+++ src/algorithms/sjf.c
@@ -1,1 +1,76 @@
-// 
+#include "../../include/scheduler.h"
+
+void sjf(Process p[], int n)
+{
+    int completed = 0;
+    int current_time = 0;
+
+    while (completed < n)
+    {
+        int shortest = -1;
+        int min_burst = 999999;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (p[i].arrival_time <= current_time &&
+                p[i].remaining_time > 0)
+            {
+                // Select smaller burst time
+            
+                if (p[i].burst_time < min_burst)
+                {
+                    min_burst = p[i].burst_time;
+                    shortest = i;
+                }
+
+                // Tie breaker: smaller PID first
+                   
+                else if (p[i].burst_time == min_burst)
+                {
+                    if (p[i].pid < p[shortest].pid)
+                    {
+                        shortest = i;
+                    }
+                }
+            }
+        }
+
+        // No process available 
+        if (shortest == -1)
+        {
+            current_time++;
+            continue;
+        }
+
+        // First response 
+        if (p[shortest].started == 0)
+        {
+            p[shortest].response_time =
+                current_time -
+                p[shortest].arrival_time;
+
+            p[shortest].started = 1;
+        }
+
+        // Waiting time 
+        p[shortest].waiting_time =
+            current_time -
+            p[shortest].arrival_time;
+
+        // Execute fully 
+        current_time += p[shortest].burst_time;
+
+        p[shortest].remaining_time = 0;
+
+        // Completion time
+        p[shortest].completion_time =
+            current_time;
+
+        // Turnaround time
+        p[shortest].turnaround_time =
+            p[shortest].completion_time -
+            p[shortest].arrival_time;
+
+        completed++;
+    }
+}
